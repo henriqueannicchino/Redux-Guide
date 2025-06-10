26157c1814d36db328358c83c07a76afe00458b8
Luis Henrique <42474086+henriqueannicchino@users.noreply.github.com>
2025-06-10 17:40:45 +0200

    Delete src/redux/cart/action-type.js

--- src/redux/cart/action-type.js
+++
@@ -1,8 +0,0 @@
-const CartActionTypes = {
-    ADD_PRODUCT: "cart/addProduct",
-    REMOVE_PRODUCT: "cart/removeProduc",
-    INCREASE_PRODUCT_QUANTITY: "cart/increaseProductQuantity",
-    DECREASE_PRODUCT_QUANTITY: "cart/decreaseProductQuantity"
-};
-
-export default CartActionTypes;
