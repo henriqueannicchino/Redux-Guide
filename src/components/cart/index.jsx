005fb2c322243f77333b18e080bcd1257ba70863
Luis Henrique <42474086+henriqueannicchino@users.noreply.github.com>
2025-06-10 16:51:51 +0200

    feat: added card redux

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,14 +1,29 @@
+import { useSelector } from "react-redux";
+
 // Styles
 import * as Styles from "./styles";
 
+import CartItem from "../cart-item/index";
+import { selectProductsTotalPrice } from "../../redux/cart/cart.selectors";
+
 const Cart = ({ isVisible, setIsVisible }) => {
   const handleEscapeAreaClick = () => setIsVisible(false);
+
+  const { products } = useSelector((rootReducer) => rootReducer.cartReducer);
+
+  const productsTotalPrice = useSelector(selectProductsTotalPrice);
 
   return (
     <Styles.CartContainer isVisible={isVisible}>
       <Styles.CartEscapeArea onClick={handleEscapeAreaClick} />
       <Styles.CartContent>
         <Styles.CartTitle>Seu Carrinho</Styles.CartTitle>
+        
+        {products.map((product, index) => (
+          <CartItem key={index} product={product} />
+        ))}
+
+        <Styles.CartTotal>Total: R${productsTotalPrice}</Styles.CartTotal>
       </Styles.CartContent>
     </Styles.CartContainer>
   );
---
+++ src/redux/cart/action-type.js
@@ -0,0 +1,8 @@
+const CartActionTypes = {
+    ADD_PRODUCT: "cart/addProduct",
+    REMOVE_PRODUCT: "cart/removeProduc",
+    INCREASE_PRODUCT_QUANTITY: "cart/increaseProductQuantity",
+    DECREASE_PRODUCT_QUANTITY: "cart/decreaseProductQuantity"
+};
+
+export default CartActionTypes;
